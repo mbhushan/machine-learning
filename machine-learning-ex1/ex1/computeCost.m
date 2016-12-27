ad6cf7689572bd09947ee0c966517b1960bb8729
manibhushan <manibhushan.cs@gmail.com>
2016-12-26 17:00:25 -0800

    computer cost function - linear regression

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -12,10 +12,9 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-
-
-
-
+H = X * theta - y;
+H = H .^ 2;
+J = sum(H) / (2 * m);
 
 % =========================================================================
 
