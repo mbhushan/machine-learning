95ddc3a73a861d38b155e8a9bc1af81debe2997e
manibhushan <manibhushan.cs@gmail.com>
2016-12-27 20:55:53 -0800

    computer cost for multi features linear regression

--- machine-learning-ex1/ex1/computeCostMulti.m
+++ machine-learning-ex1/ex1/computeCostMulti.m
@@ -13,9 +13,9 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+H = X * theta - y;
+H = H .^ 2;
+J = sum(H) / (2 * m);
 
 % =========================================================================
 
